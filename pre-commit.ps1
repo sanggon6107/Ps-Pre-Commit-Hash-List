cbadb70d93cb4393254e48173f733c5e7a20ee74
sanggon6107 <sanggon6107@naver.com>
2024-02-25 22:43:23 +0900

    Feat : Change ouput format from csv to json Set output format to json that consists of dll file name and md5 hash

--- pre-commit.ps1
+++ pre-commit.ps1
@@ -2,5 +2,5 @@ $hashPath=".\\folder1"
 if (Test-Path $hashPath\hashList.json) {
     Remove-Item $hashPath\hashList.json
 }
-Get-ChildItem -Path . -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | % {$_.Path.replace("\","/").split("/")[-1] + "," + $_.Hash} > $hashPath\hashList.csv
+Get-ChildItem -Path . -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | Select-Object Hash, @{Name='Name';Expression={$_.Path.replace("\","/").split("/")[-1]}} | ConvertTo-Json > hashList.json
 git add $hashPath\hashList.json
