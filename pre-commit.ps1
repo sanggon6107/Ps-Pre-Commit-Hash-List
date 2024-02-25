bda802f4e66552315eb362b1807c8776d2ec5a45
sanggon6107 <sanggon6107@naver.com>
2024-02-25 23:22:28 +0900

    Fix : Fix a bug Fix a directory of json file

--- pre-commit.ps1
+++ pre-commit.ps1
@@ -1,6 +1,7 @@
 $hashPath=".\\folder1"
+$filePath="."
 if (Test-Path $hashPath\hashList.json) {
     Remove-Item $hashPath\hashList.json
 }
-Get-ChildItem -Path . -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | Select-Object Hash, @{Name='Name';Expression={$_.Path.replace("\","/").split("/")[-1]}} | ConvertTo-Json > hashList.json
+Get-ChildItem -Path $filePath -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | Select-Object Hash, @{Name='Name';Expression={$_.Path.replace("\","/").split("/")[-1]}} | ConvertTo-Json > $hashPath\hashList.json
 git add $hashPath\hashList.json
