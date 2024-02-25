e2ce67555ce3fc3e3c255c82fd52101542c53e07
sanggon6107 <sanggon6107@naver.com>
2024-02-25 22:07:58 +0900

    Feat : Change output format from json to csv

--- pre-commit.ps1
+++ pre-commit.ps1
@@ -2,5 +2,5 @@ $hashPath=".\\folder1"
 if (Test-Path $hashPath\hashList.json) {
     Remove-Item $hashPath\hashList.json
 }
-Get-ChildItem -Path . -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | Select Hash, Path | ConvertTo-Json > $hashPath\hashList.json
+Get-ChildItem -Path . -Recurse -Filter *.dll | Get-FileHash -Algorithm MD5 | % {$_.Path.replace("\","/").split("/")[-1] + "," + $_.Hash} > $hashPath\hashList.csv
 git add $hashPath\hashList.json
